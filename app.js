61bbdea0a8849881395b81342b42333372dba70c
Rhodanthe1116 <z0916945857@gmail.com>
2020-10-30 01:01:27 +0800

    fix heroku build bug

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ mongoose.connect(uri, {
 }
 );
 const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
   console.log("MongoDB database connection established successfully");
 })
